5b74afb7f677d76a393cdfb5c37cda431c3e335b
Fenz-art <kushangatwork@gmail.com>
2025-12-19 02:55:48 +0530

    fix build: type-only imports

--- src/pages/FillFormPage.tsx
+++ src/pages/FillFormPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react'
+import type{ FormEvent } from 'react'
+import {  useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useFileUpload } from '../hooks/useFileUpload'
 import { api } from '../lib/api'
